f33d56201508db401c2fbdd0c410f874450207d4
polirritmico <ebray187@gmail.com>
2022-11-17 20:58:05 -0300

    Implemented load_file_data unittest

--- test/test_unit_sunai.py
+++ test/test_unit_sunai.py
@@ -9,14 +9,19 @@ from src.sunai import Sunai
 #@unittest.skip
 class TestSunai(unittest.TestCase):
     def setUp(self):
-        pass
+        self.sunai = Sunai()
 
 
     #@unittest.skip
-    def test_description(self):
-        expected = False
-        output = True
+    def test_load_file_data(self):
+        filepath = "test/cases/data_plantas_python_2.xlsx"
+        expected_first_value = 100358904
+        expected_last_value = 112923304
 
-        self.assertEqual(expected, output)
+        self.sunai.load_xlsx(filepath)
+        active_energy = self.sunai.get_active_energy()
+
+        self.assertEqual(expected_first_value, active_energy[0])
+        self.assertEqual(expected_last_value, active_energy[-1])
 
 
