8e42283a45432a19cb2677efe84f31e5086af9c9
polirritmico <ebray187@gmail.com>
2022-11-18 18:23:25 -0300

    Code refaction and added functions:
    Change class variable names, active_energy and active_power now have the
    id_i column so we can check the data by inversor. Also added some getter
    functions to get min, max and sum values.
    
    Added the make_summary_txt function to collect the data and format the
    txt file.
    
    Update all the unittest regarding the udpated pandas dataframes.

--- src/power_plant.py
+++ src/power_plant.py
@@ -9,10 +9,12 @@ import datetime
 
 
 class PowerPlant():
-    def __init__(self, filename="default"):
+    def __init__(self, filename="default", outdir=""):
         self.filename = filename
-        self.file_data = None
+        self.grap_filename = None
+        self.output_dir = self.default_output_dir() if outdir == "" else outdir
 
+        self.datafile = None
         self.active_energy = None
         self.active_power = None
         self.dates = None
@@ -23,15 +25,15 @@ class PowerPlant():
 
     def load_file(self):
         try:
-            self.file_data = pd.read_excel(self.filename).fillna(0)
+            datafile = pd.read_excel(self.filename, parse_dates=True).fillna(0)
         except Exception as err:
             print("ERROR: Can't read the file: ")
             raise err
+
         # Parse the data
-        #self.file_data.info()
-        self.active_energy = self.file_data["active_energy_im"]
-        self.active_power = self.file_data["active_power_im"]
-        self.dates = self.file_data["fecha_im"]
+        self.datafile = datafile
+        self.active_energy = datafile[["id_i", "active_energy_im"]]
+        self.active_power = datafile.set_index("fecha_im")[["id_i", "active_power_im"]]
 
 
     def make_graph(self):
@@ -41,12 +43,16 @@ class PowerPlant():
             - Eje y active power
             - Guardar archivo
         """
-        self.graph_data = pd.DataFrame(self.active_power, self.dates)
-        #df['date'] = pd.to_datetime(df['date'])
-        #df.groupby(df['date'].dt.date)
 
-        self.graph_line = self.graph_data.plot(
-            kind="line", grid=True, ylabel="Active power", xlabel="Fecha"
+        self.graph_line = self.active_power.plot(
+                kind="line",
+                title="Active power by inversor",
+                grid=True,
+                #ylabel="Active power",
+                xlabel="Timestamp",
+                #x_compat=True,
+                #color="tab:orange",
+                #legend=False,
         )
 
 
@@ -55,29 +61,74 @@ class PowerPlant():
 
 
     def get_active_energy_val_by_index(self, index) -> int():
-        return self.active_energy.values[index]
+        return self.active_energy.values[index][1]
 
 
     def get_date_by_index(self, index) -> str():
-        date = self.dates.values[index]
+        print("HERE")
+        date = self.datafile["fecha_im"].values[index]
+        print(date)
+        #date = self.dates.values[index]
         return pd.to_datetime(str(date)).strftime("%Y-%m-%d %H:%M:%S")
-        
-
-# =============================================================================
 
 
-    def get_active_power_sum(self):
+    def min_active_energy(self) -> int():
+        return self.active_energy.min()[1]
+
+
+    def max_active_energy(self) -> int():
+        return self.active_energy.max()[1]
+
+
+    def active_power_sum_by_day(self):
+        return int(self.active_power["active_power_im"].sum())
+
+
+    def default_output_dir(self):
+        #self.filename → test/cases/data_plantas_python_1_1.xlsx
+        subfolder = os.path.splitext(self.filename)[0].split('/')[-1]
+        outdir = "output" + '/' + subfolder + '/'
+        return outdir
+
+
+    def check_output_dir(self):
+        """Create the output directory if it doesn't exist"""
+        try:
+            os.makedirs(os.path.dirname(self.get_outpath()), exist_ok=True)
+        except Exception as err:
+            print("ERROR: Can't read/write the output folder")
+
+
+
+    def make_summary_txt(self):
         """
         Generar txt con:
-            - Suma por día del active power
-            - Valor mínimo de active energy
-            - Valor máximo de active energy
-            - Path al archivo del gráfico
+            - [x] Suma por día del active power
+            - [x] Valor mínimo de active energy
+            - [x] Valor máximo de active energy
+            - [x] Path al archivo del gráfico
         """
-        pass
+        txt_content = [
+            str(self.active_power_sum_by_day()),
+            str(self.min_active_energy()),
+            str(self.max_active_energy()),
+            #self.filename, 
+        ]
+        file_data = "\n".join(txt_content)
+
+        self.check_output_dir()
+        filename = self.output_dir + "daily_summary.txt"
+        try:
+            with open(filename, encoding="utf-8") as file:
+                file.write(file_data)
+        except Exception as err:
+            print("ERROR: Can't write the summary file")
+            raise err
+
+        return file_data
 
 
-    def dummy(self):
+    def console_output(self):
         """
         Output consola:
             - Suma total del active power por día de todas las plantas
--- test/test_unit_power_plants.py
+++ test/test_unit_power_plants.py
@@ -13,22 +13,24 @@ class TestPowerPlant(unittest.TestCase):
         self.power_plant.filename = "test/cases/data_plantas_python_1_1.xlsx"
 
 
+    #@unittest.skip
     def test_load_xlsx_file_dummy(self):
-        expected = "    fecha_im  active_energy_im  active_power_im\n0 2022-11-17         123456789        987654321"
+        expected = "   id_i   fecha_im  active_energy_im  active_power_im\n0    42 2022-11-17         123456789        987654321"
         self.power_plant.filename = "test/cases/dummy.xlsx"
         self.power_plant.load_file()
-        output = str(self.power_plant.file_data)
+        output = str(self.power_plant.datafile)
+        print(output)
 
         self.assertEqual(expected, output)
 
 
     #@unittest.skip
     def test_load_xlsx_file_active_energy_data(self):
-        self.power_plant.filename = "test/cases/data_plantas_python_2.xlsx"
         expected_first_value = 100358904
         expected_mid_value = 100384504 # row 288 -> index 286
         expected_last_value = 112923304
 
+        self.power_plant.filename = "test/cases/data_plantas_python_2.xlsx"
         self.power_plant.load_file()
         output_first_value = self.power_plant.get_active_energy_val_by_index(0)
         output_mid_value = self.power_plant.get_active_energy_val_by_index(286)
@@ -56,29 +58,56 @@ class TestPowerPlant(unittest.TestCase):
 
 
     @unittest.skip
-    def test_active_power_day_sum(self):
+    def test_make_summary_txt_data(self):
+        expected="3664916\n46870800\n76978296"
         self.power_plant.load_file()
+        output = self.power_plant.make_summary_txt()
+
+        self.assertEqual(expected, output)
+
+
+    #@unittest.skip
+    def test_active_power_sum_by_day(self):
         expected = 3664916
-        output = self.power_plant.get_active_power_day_sum()
-
-
-    @unittest.skip
-    def test_active_get_max_active_energy(self):
         self.power_plant.load_file()
-        expected = 46978296
-        output = self.power_plant.get_max_active_energy()
+        output = self.power_plant.active_power_sum_by_day()
 
         self.assertEqual(expected, output)
 
 
-    @unittest.skip
-    def test_active_get_min_active_energy(self):
+    #@unittest.skip
+    def test_active_max_active_energy(self):
+        expected = 76978296
         self.power_plant.load_file()
+        output = self.power_plant.max_active_energy()
+
+        self.assertEqual(expected, output)
+
+
+    #@unittest.skip
+    def test_active_min_active_energy(self):
         expected = 46870800
-        output = self.power_plant.get_min_active_energy()
+        self.power_plant.load_file()
+        output = self.power_plant.min_active_energy()
 
         self.assertEqual(expected, output)
 
+
+    #@unittest.skip
+    def test_default_output_dir(self):
+        expected = "output/dummy/"
+
+        self.power_plant.filename = "test/cases/dummy.xlsx"
+        output = self.power_plant.default_output_dir()
+        self.assertEqual(expected, output)
+
+        self.power_plant.filename = "any_folder/dummy.xlsx"
+        output = self.power_plant.default_output_dir()
+        self.assertEqual(expected, output)
+
+        self.power_plant.filename = "dummy.xlsx"
+        output = self.power_plant.default_output_dir()
+        self.assertEqual(expected, output)
 
 
     @unittest.skip
