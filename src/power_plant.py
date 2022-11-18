96287fd2fdca2695fa9475bf9e80e880b4019a5c
polirritmico <ebray187@gmail.com>
2022-11-17 22:19:47 -0300

    load_file parse data refaction

--- src/power_plant.py
+++ src/power_plant.py
@@ -23,13 +23,12 @@ class PowerPlant():
             print("ERROR: Can't read the file: ")
             raise err
         # Parse the data
-        self.active_energy = pd.DataFrame(self.file_dataframe,
-                                          columns=["active_energy_im"])
-        self.dates = pd.DataFrame(self.file_dataframe, columns=["fecha_im"])
+        self.active_energy = self.file_dataframe["active_energy_im"]
+        self.dates = self.file_dataframe["fecha_im"]
 
 
     def get_active_energy_val_by_index(self, index) -> list():
-        return self.active_energy.iloc[index]["active_energy_im"]
+        return self.active_energy.values[index]
 
         
 
