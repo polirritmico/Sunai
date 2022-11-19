55dc3fdf3d811de6156649f82ffb29d9949fc117
polirritmico <ebray187@gmail.com>
2022-11-19 20:32:54 -0300

    Rename Sunai to SunaiChallenge class

--- src/sunai.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-class Sunai():
-    def __init__(self):
-        plants_collection = []
-
---
+++ src/sunai_challenge.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+from src.power_plant import PowerPlant
+from src.day import Day
+
+
+class SunaiChallenge():
+    def __init__(self):
+        pass
+
+
