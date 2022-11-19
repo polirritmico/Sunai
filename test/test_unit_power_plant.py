4618ed1be16d11b4d80aa2fb3a3db1862a7543cb
polirritmico <ebray187@gmail.com>
2022-11-19 20:29:52 -0300

    Added test_unit_power_plant.py

---
+++ test/test_unit_power_plant.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import os
+
+from src.power_plant import PowerPlant
+
+
+#@unittest.skip
+class TestPowerPlant(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+
