a498032207ad12adabc7596f1a4434a08a11d252
polirritmico <ebray187@gmail.com>
2022-11-21 11:18:11 -0300

    Simple changes to performance test

--- performance_test.py
+++ performance_test.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import timeit
+import time
 
-test_code = """
 from src.sunai_challenge import SunaiChallenge
 
-sunai = SunaiChallenge()
-sunai.run(["test/performance_full", "test/performance_output"])
-"""
-
 if __name__ == "__main__":
-    t = timeit.timeit(test_code, number=1)
-    print("Repetitions: 1\nRuntime: {0:.2f} s".format(t))
+    print("Simple Performance Test\n=======================\n")
+    sunai = SunaiChallenge()
+    print("Starting test...", end="")
+    start = time.time()
+    sunai.run(["test/performance_pretest", "test/performance_output"])
+    end = time.time()
+    print(" Done")
+
+    t = end - start
+    h, rem = divmod(t, 3600)
+    min, sec = divmod(rem, 60)
+    processed_files = sunai.processed_files_count()
+    print("Processed input files: {}".format(processed_files))
+    print("Runtime: {:0>2}:{:05.2f}".format(int(min), sec))
+    t = int(t * (1000 / processed_files))
+    h, rem = divmod(t, 3600)
+    min, sec = divmod(rem, 60)
+    print("Estimated time for 1000 repetitions: {:0>2}:{:0>2}:{:0>2}"
+          .format(int(h), int(min), int(sec)))
